91a222bef172b0a2791bdd5557908e5549daf2dd
dsgdsg <nazirsavisko@gmail.com>
2025-05-20 22:10:45 +0300

    init

---
+++ src/main/java/pizza/maker/App.java
@@ -0,0 +1,13 @@
+package pizza.maker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
